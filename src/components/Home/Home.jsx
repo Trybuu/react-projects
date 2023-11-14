b3e744aa31793e5e685f948e4275a59f07b6c7fb
Marek Trybuła <trybu001@gmail.com>
2023-11-14 19:25:53 +0100

    House content placeholders, pricing-component project assets

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,44 @@ import './Home.css'
 export default function Home() {
   return (
     <div id="home">
-      <h1>react-projects</h1>
+      <h2>Cel powstania tej strony?</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, assumenda
+        a maxime aspernatur asperiores ad esse quibusdam nam explicabo autem,
+        omnis, commodi animi rerum reprehenderit numquam alias quam quas
+        necessitatibus reiciendis exercitationem. Itaque illo officiis amet
+        consectetur quo minima debitis doloremque cupiditate culpa quam
+        doloribus maxime eum facilis, rerum inventore repudiandae sunt nulla
+        corrupti consequuntur quidem ex sint nobis. Reiciendis nostrum deserunt
+        temporibus distinctio illum? Amet accusantium totam quod beatae?
+        Veritatis deserunt architecto quos alias error soluta asperiores!
+        Dolorum repellat quos amet, animi sit nam, eos temporibus unde beatae
+        dignissimos veniam ipsam, facilis vel. Nisi ipsam officia quidem sit
+        aspernatur!
+      </p>
+      <h2>Jak korzystać?</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, assumenda
+        a maxime aspernatur asperiores ad esse quibusdam nam explicabo autem,
+        omnis, commodi animi rerum reprehenderit numquam alias quam quas
+        necessitatibus reiciendis exercitationem. Itaque illo officiis amet
+        consectetur quo minima debitis doloremque cupiditate culpa quam
+        doloribus maxime eum facilis, rerum inventore repudiandae sunt nulla
+        corrupti consequuntur quidem ex sint nobis. Reiciendis nostrum deserunt
+        temporibus distinctio illum? Amet accusantium totam quod beatae?
+        Veritatis deserunt architecto quos alias error soluta asperiores!
+        Dolorum repellat quos amet, animi sit nam, eos temporibus unde beatae
+        dignissimos veniam ipsam, facilis vel. Nisi ipsam officia quidem sit
+        aspernatur!
+      </p>
+      <h2>Kim jestem?</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
+        consequuntur esse aut nisi itaque, et dolores placeat libero ut autem,
+        nihil excepturi corrupti expedita adipisci ipsa cupiditate perspiciatis.
+        Maiores officiis dignissimos totam iusto veniam laborum beatae ipsum
+        doloribus accusamus excepturi.
+      </p>
     </div>
   )
 }
