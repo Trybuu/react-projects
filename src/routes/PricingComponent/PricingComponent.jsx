93abe25c00c75cf932ba6ac9df951a70441a48fc
Marek Trybuła <trybu001@gmail.com>
2023-11-14 21:26:32 +0100

    PricingSwitch component made

--- src/routes/PricingComponent/PricingComponent.jsx
+++ src/routes/PricingComponent/PricingComponent.jsx
@@ -1,13 +1,30 @@
-import PricingSwitch from './PricingSwitch/PricingSwitch'
+import { useState } from 'react'
 
+import PricingSwitch from './PricingSwitch/PricingSwitch'
 import bgBottom from '../../assets/pricing-component files/images/bg-bottom.svg'
+import bgTop from '../../assets/pricing-component files/images/bg-top.svg'
 import './PricingComponent.css'
 
 export default function PricingComponent() {
+  const [pricing, setPricing] = useState('Monthly')
+
+  function handlePricingSwitch() {
+    setPricing(pricing === 'Monthly' ? 'Annually' : 'Monthly')
+  }
+
   return (
     <div id="pricing-component">
-      <img src={bgBottom} alt="" />
-      <PricingSwitch />
+      <PricingSwitch onSetPricing={handlePricingSwitch} pricing={pricing} />
+      <img
+        src={bgBottom}
+        alt="stripes - decoration"
+        className="decoration-img bottom"
+      />
+      <img
+        src={bgTop}
+        alt="stripes - decoration"
+        className="decoration-img top"
+      />
     </div>
   )
 }
--- src/routes/PricingComponent/PricingSwitch/PricingSwitch.jsx
+++ src/routes/PricingComponent/PricingSwitch/PricingSwitch.jsx
@@ -1,3 +1,21 @@
-export default function PricingSwitch() {
-  return <h1>Pricing Switch</h1>
+import './PricingSwitch.css'
+
+export default function PricingSwitch({ onSetPricing, pricing }) {
+  return (
+    <>
+      <h1>Our Pricing</h1>
+      <div className="pricing-switch">
+        <span>Annually</span>
+        <button
+          className={`pricing-switch-btn ${
+            pricing === 'Monthly' ? 'active' : null
+          }`}
+          onClick={onSetPricing}
+        >
+          <div className="circle"></div>
+        </button>
+        <span>Monthly</span>
+      </div>
+    </>
+  )
 }
